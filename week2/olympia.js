ac35ae2e8905f1c08555fd144b9f1ccdf0148a9a
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-01-01 22:56:19 -0800

    olympia commit

---
+++ week2/olympia.js
@@ -0,0 +1,109 @@
+// Preliminaries
+
+var x = 5;
+if (x > 3){
+    console.log("is greater than");
+    
+}
+
+
+var cat = 3;
+
+ if (cat = 3){
+    console.log("is the length ");
+ }
+
+ var dog = 1;
+ var cat = 1;
+
+ dog === cat ? $Result = "cat is equal to the dog" : $Result = "not the same";
+    console.log($Result);
+
+//* Bronze Medal
+
+
+var person = {
+    name: "Bobby",
+    age: 12,
+    nameB: true,
+}
+if (person.age > 18){
+    console.log("Ben is allowed to the movie");
+}
+
+if (person.name [0] === "B") {
+        console.log("Ben is allowed to see a movie");
+}
+else {
+    console.log("Ben is not allowed to see a movie");
+}
+if (person.name[0] === "B" && person.age >= 18) {
+    console.log("Ben is allowed to see a movie");
+}
+else {
+console.log("Ben is not allowed to see a movie")
+}
+
+//* Silver Medal
+
+if ( 1 === "1" )  {
+    console.log("strict");    
+}
+else if ( 1 == "1") {
+    console.log("abstract");
+}
+else {
+    console.log("not equal at all");
+}
+if (1 <= 2 && 2 === 4) {
+    console.log("yes");
+}
+else {
+    console.log("no");
+}
+ //* Gold Medal
+
+ if ( typeof "dog" === "string" ) {
+     console.log("dog is a string");
+ }
+else {
+    console.log("dog is not a string");
+}
+if ( typeof "true" === "boolean" ) {
+    console.log("true is a boolean");
+}
+else {
+   console.log("true is not a boolean");
+} 
+
+let variable;
+
+if (typeof variable !== "undefined") {
+    console.log("variable is defined");
+}
+else {
+    console.log("variable is not defined");
+}
+
+if ( "s" > 12 ) {
+    console.log("s is a true statement");
+}
+else {
+    console.log("s is a false statement");
+}
+
+if ( "r" < 30 ) {
+    console.log("r is a true statement");
+}
+else {
+    console.log("r is a false statement");
+}
+
+if ( "p" > 1 ) {
+    console.log("p is a true statement");
+}
+else {
+    console.log("p is a false statement");
+}
+
+var myNum = 10 ? console.log("number is even") : console.log("number is odd");
