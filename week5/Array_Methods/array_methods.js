ed8503a8f72f51bdcad8d50b975abd5abe423c06
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-01-27 16:56:39 -0800

    update array_methods.js

--- week5/Array_Methods/array_methods.js
+++ week5/Array_Methods/array_methods.js
@@ -11,13 +11,13 @@ var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
     var indexofOrange = fruit.indexOf("orange");
 
 //4. Add that number to the end of the fruit array.
-    fruit.push(1);
+    fruit.push(indexofOrange);
 
 //5. Use the length property to find the length of the vegetable array.
     vegetables.length;
 
 //6. Add that number to the end of the vegetable array.
-    vegetables.push(3);
+    vegetables.push(vegetables.length);
 
 //7. Put the two arrays together into one array. Fruit first. Call the new Array "food".
     var food = fruit.concat(vegetables);
--- week5/Higher_Arrays/higher_arrays.js
+++ week5/Higher_Arrays/higher_arrays.js
@@ -1,11 +1,11 @@
-/* A
-1. Given an array of numbers, return a new array that has only the numbers that are 5 or greater.*/
+// AUse the built-in array method .filter() to solve all of these problems:
+//1. Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 
 function fiveAndGreaterOnly(arr) {
     return arr.filter(num => num > 5);
 }
 
-//console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
+console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
 
 //2. Given an array of numbers, return a new array that only includes the even numbers.
 
@@ -13,17 +13,17 @@ function evensOnly(arr) {
     return arr.filter(num => num % 2 === 0);
     
 }
-//console.log(evensOnly ([3, 6, 8, 2]));
+console.log(evensOnly ([3, 6, 8, 2]));
 
 
-// B
+// B. Use the built-in .map() method on arrays to solve all of these problems:
 // 1. Make an array of numbers that are doubles of the first array
 
 function doubleNumbers(arr) {
     return arr.map(num => num * 2);
 }
 
-//console.log(doubleNumbers([2, 5, 100]));
+console.log(doubleNumbers([2, 5, 100]));
 
  //2. Take an array of numbers and make them strings
 
@@ -31,7 +31,7 @@ function stringItUp(arr) {
     return arr.map(num => num.toString());
 }
 
-//console.log(stringItUp([2, 5, 100]));
+console.log(stringItUp([2, 5, 100]));
 
 // 3. Capitalize each of an array of names
 
@@ -42,7 +42,7 @@ function capitalizeNames(arr) {
 console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
 
 
-// C.
+// C.Use the built-in .reduce() method on arrays to solve all of these problems:
 
 // 1. Turn an array of numbers into a total of all the numbers
 
