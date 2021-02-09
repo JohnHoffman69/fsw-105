85abd3af7f77e6cb16c7be090d095b6b220af205
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-02-08 21:14:53 -0800

    update to Higher_Arrays

--- week5/Higher_Arrays/higher_arrays.js
+++ week5/Higher_Arrays/higher_arrays.js
@@ -10,23 +10,23 @@ function fiveAndGreaterOnly(arr) {
 }
 
 
-console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
+//console.log(fiveAndGreaterOnly([3, 6, 8, 2]));
 
 // 2) Given an array of numbers, return a new array   that only includes the even numbers.
 //method .filter()
 
 
-function evensOnly(arr) {
-    return arr.filter(function(num){
+function evensOnly(arr)
+{
+    const result = arr.filter(function(num){
         if (num % 2 == 0)
             return num;
-
-    });
-    
+    }
+    )
+    return result;
 }
+//console.log(evensOnly([3, 6, 8, 2]));
 
-
-console.log(evensOnly([3, 6, 8, 2]));
 
 
 
@@ -35,13 +35,15 @@ console.log(evensOnly([3, 6, 8, 2]));
 // 1. Make an array of numbers that are doubles of the first array
 
 function doubleNumbers(arr) {
-    const reult = arr.map(function(num){
-        return num * 2;
-    });
+
+    const result = arr.map(function(num) {
+        return num + num;
+    }
+    )
     return result;
 }
+//console.log(doubleNumbers([2, 5, 100]));
 
-console.log(doubleNumbers([2, 5, 100]));
 
  //2. Take an array of numbers and make them strings
 
@@ -52,7 +54,7 @@ function stringItUp(arr) {
     return result;
 }
 
-console.log(stringItUp([2, 5, 100]));
+//console.log(stringItUp([2, 5, 100]));
 
 // 3. Capitalize each of an array of names
 
@@ -64,7 +66,7 @@ function capitalizeNames(arr) {
 }
 
 
-console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
+//console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
 
 
 // C.Use the built-in .reduce() method on arrays to solve all of these problems:
@@ -80,7 +82,7 @@ function total(arr) {
 }
 
 
-console.log(total([1, 2, 3]));
+//console.log(total([1, 2, 3]));
 
 // 2. Turn an array of numbers into a long string of all those numbers.
 
@@ -92,7 +94,7 @@ function stringConcat(arr) {
     return result;
 }
 
-console.log(stringConcat([1, 2, 3]));
+//console.log(stringConcat([1, 2, 3]));
 
 // 3. Turn an array of voter objects into a count of how many people voted
 
@@ -107,23 +109,37 @@ function totalVotes(arr) {
     }, 0)
 
 }    
+function totalVotes(arr) 
+{
+    const result = arr.reduce(function(total, votes) {
+        if (votes.voted == true) {
+            total++
+        }
+        return total;
+    
+    }, 0
+    )
+    return result;
+}
+
 var voters = [
-    { name: "Bob", age: 30, voted: true },
-    { name: "Jake", age: 32, voted: true },
-    { name: "Kate", age: 25, voted: false },
-    { name: "Sam", age: 20, voted: false },
-    { name: "Phil", age: 21, voted: true },
-    { name: "Ed", age: 55, voted: true },
-    { name: "Tami", age: 54, voted: true },
-    { name: "Mary", age: 31, voted: false },
-    { name: "Becky", age: 43, voted: false },
-    { name: "Joey", age: 41, voted: true },
-    { name: "Jeff", age: 30, voted: true },
-    { name: "Zack", age: 19, voted: false }
+    {name: "Bob" , age: 30, voted: true},
+    {name: "Jake" , age: 32, voted: true},
+    {name: "Kate" , age: 25, voted: false},
+    {name: "Sam" , age: 20, voted: false},
+    {name: "Phil" , age: 21, voted: true},
+    {name: "Ed" , age: 55, voted: true},
+    {name: "Tami" , age: 54, voted: true},
+    {name: "Mary" , age: 31, voted: false},
+    {name: "Becky" , age: 43, voted: false},
+    {name: "Joey" , age: 41, voted: true},
+    {name: "Jeff" , age: 30, voted: true},
+    {name: "Zack" , age: 19, voted: false}
+
 ];
 
+//console.log(totalVotes(voters));
 
-console.log(totalVotes(voters));
 
 
 
